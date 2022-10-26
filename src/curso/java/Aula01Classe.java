f1f28777c34d00b38de8c8236b8b430b2582aaa2
albertomoiseis <contatoalbertomoiseis@gmail.com>
2022-10-25 22:59:17 -0300

    Curso de Java Orientado a Objetos

---
+++ src/curso/java/Aula01Classe.java
@@ -0,0 +1,27 @@
+package curso.java;
+
+public class Aula01Classe {
+    public static void main(String[] args) {
+        // Descrição de um conjunto de objetos do mesmo tipo
+        /*
+        Avião
+            Características:
+                Fabricante;
+                Modelo;
+                Cor;
+                Autonomia;
+                Quantidade de Turbinas;
+            Comportamentos:
+                Ligar Turbinas;
+                Desligar Turbinas;
+                Aumentar Potência;
+                Diminuir Potência;
+                Mudar Direção;
+                Abastecer;
+        * */
+        /*
+        Instanciação de Objetos;
+        Um objeto é uma instancia única de um tipo especifico "classe" se comunicando atravez dos seus métodos
+        */
+    }
+}
