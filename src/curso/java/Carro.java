8d8cee6f92f73c401be58786daca3852f4e50eb4
albertomoiseis <contatoalbertomoiseis@gmail.com>
2022-10-26 01:30:29 -0300

    Curso de Java OO: Classe Carro

---
+++ src/curso/java/Carro.java
@@ -0,0 +1,14 @@
+package curso.java;
+/*
+public -> modificador de acesso
+* */
+
+// Classe Carro
+public class Carro {
+
+    // Atributos
+    String fabricante;
+    String modelo;
+    String cor;
+    int anoFabricacao;
+}
